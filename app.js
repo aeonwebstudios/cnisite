b98f63a89c23422d70b311510f0ec48c914805ff
CNISS <aeonwebstudios@gmail.com>
2018-09-10 14:41:09 +0300

    appfile update

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var app = express();
 
 
 //setting up the view engine
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine', 'pug');
+app.set('views',path.join(__dirname,'src'));
+//app.set('view engine', 'pug');
 
 
 
@@ -21,16 +21,34 @@ app.set('view engine', 'pug');
 //body-parser middleWare
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname,	'public')));
+app.use(express.static(path.join(__dirname,	'src')));
 
 
 //our routes
- app.get('/',function(req,res){
-     res.render('index');
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/about.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/discover.html'));
+});
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/projects.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/gallery.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/contact.html'));
+});
+
+
+app.listen(process.env.PORT || 3000, function(){
+   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
  });
-
-
-
-
-app.listen(3001)
-console.log("server is runing on port 3001");
